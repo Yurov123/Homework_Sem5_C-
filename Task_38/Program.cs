84412a0e400425a1d5f09ee7cee413711603c4aa
Konstantin <constantin8271@gmail.com>
2022-02-15 21:59:18 +0300

    Task_38

---
+++ Task_38/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму чисел одномерного массива, стоящих на нечётной позиции.
+
+
+var rnd = new Random();
+var arr = new int[9];
+ 
+for (int i = 0; i < arr.Length; i++)
+    Console.Write("{0}\t", arr[i] = rnd.Next(3,50 ));
+    Console.WriteLine();
+ 
+var odd = arr.Where((element, index) => index % 2 != 0);
+           
+Console.WriteLine("Odd. Quantity: {0}. Summ: {1}.", odd.Count(), odd.Sum());
+Console.ReadKey();
+        
+    
